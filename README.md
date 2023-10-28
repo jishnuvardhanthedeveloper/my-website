fbab3ed7c6e4c5036a8fbb75c0d1495af026a14b
Jishnu Vardhan Reddy <jishnuvardhan.developer@gmail.com>
2023-10-28 18:55:25 +0100

    final

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tailwind CSS and Next.js Portfolio
+
+This is a Portfolio Website
+
+## How it works
+
+Fork or download the repo and change whatever you need to change for your needs.
+
+## Running Locally
+
+Can run the application in VS Code or a terminal and it will be available at `http://localhost:3000`.
+
+```bash
+npm install
+npm run dev
+```
---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>Jishnu Karri</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Jishnu's Portfolio" />
+      <meta name="tags" content="jishnu,karri,vardhan,reddy"/>
+      <link rel="icon" type="image/png" href="IMG_0510.jpg" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
+      />
+    </>
+  );
+}
